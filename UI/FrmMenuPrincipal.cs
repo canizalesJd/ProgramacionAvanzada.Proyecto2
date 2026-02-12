0a4c74d409ae0ba2ae71dea45e9d2d0d9fc46d1f
Jose Canizales <canizalesjd@gmail.com>
2026-02-12 00:38:17 -0600

    Adding new UI folder and backin up old UI layer

---
+++ UI/FrmMenuPrincipal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows.Forms;
+
+/*
+ * Universidad Estatal a Distancia (UNED)
+ * Cuatrimestre: I Cuatrimestre 2026
+ * Proyecto: Proyecto 1 - Programación Avanzada | AutoMarket
+ * Descripción: Programa de gestión de ventas de vehículos
+ * Estudiante: Jose David Canizales Azocar
+ * Fecha: Febrero 2026
+ */
+
+namespace CapaPresentacion
+{
+    public partial class FrmMenuPrincipal : Form
+    {
+        public FrmMenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void registrarCategoriaVehiculoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FrmRegistrarCategoriaVehiculo frm = new FrmRegistrarCategoriaVehiculo();
+            frm.Show();
+        }
+    }
+}
